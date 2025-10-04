296d4c17158bee6eccc00fb21b0bfa9e87fbbd4e
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-10-05 00:24:49 +0700

    Refactor code structure for improved readability and maintainability

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\OwnerPanelProvider::class,
     App\Providers\Filament\SuperadminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
